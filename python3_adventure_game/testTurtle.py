174e4486d3a4ee333d17d4d8d116e56b67ca03fc
lucasWzhang <georgezhang012@gmail.com>
2022-11-10 14:19:10 -0500

    completed adventure_game

---
+++ python3_adventure_game/testTurtle.py
@@ -0,0 +1,38 @@
+# import turtle
+
+# def drawTurtle():
+#     amy=turtle.Turtle()
+
+#     amy.color("red")
+
+#     for i in range(6):
+#         amy.forward(100)
+#         amy.right(60)
+#     amy.hideturtle
+
+
+# def main():
+#     drawTurtle()
+
+
+
+# if __name__ == "__main__":
+#     main()
+
+import turtle
+
+amy=turtle.Turtle()
+
+amy.color("red")
+amy.forward(100)
+amy.right(90)
+
+amy.color("yellow")
+amy.forward(100)
+amy.right(90)
+amy.color("yellow")
+amy.forward(100)
+amy.right(90)
+amy.color("green")
+amy.forward(100)
+amy.right(90)
